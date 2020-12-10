56d2ce1f73d0fe1db351abd028e0f8c47974df8d
Pradip Budhathoki <pradipbudhathoki3@gmail.com>
2020-12-10 17:22:00 +0545

    setting

--- newproject/settings.py
+++ newproject/settings.py
@@ -125,7 +125,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'bbcdjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'WARMACHINEROX'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
